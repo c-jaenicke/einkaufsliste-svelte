a14ad87aedd2885b778c3e40fb5018e22e2b8d86
c-jaenicke <me@chrisj.de>
2023-11-17 15:24:56 +0100

    improve pet form

--- src/routes/pet/+page.svelte
+++ src/routes/pet/+page.svelte
@@ -4,14 +4,20 @@
 
 	export let data;
 
-	function getTime() {
+	function getTimeFed() {
 		let date = new Date(data.pet.fed_at * 1000);
+
+		return date.toLocaleTimeString();
+	}
+
+	function getTimeInside() {
+		let date = new Date(data.pet.inside_at * 1000);
 
 		return date.toLocaleTimeString();
 	}
 </script>
 
-<PageTitle title="Neuen Eintrag anlegen" />
+<PageTitle title="Katze" />
 
 <div class="flex justify-between">
 	<a href="/new-item" class="invisible" />
@@ -21,7 +27,7 @@
 	</a>
 </div>
 
-<h2 class="h3">Zuletzt um {getTime()} <b>{data.pet.amount_fed}</b> gefressen.</h2>
+<h2 class="h3">Zuletzt um {getTimeFed()} <b>{data.pet.amount_fed}</b> gefressen.</h2>
 
 <form method="POST" action="?/fed">
 	<label class="label">
@@ -31,22 +37,22 @@
 			<option value="sehr wenig">Sehr Wenig</option>
 			<option value="wenig">Wenig</option>
 			<option value="naja">Naja</option>
-			<option value="viel">Viel</option>
+			<option selected value="viel">Viel</option>
 			<option value="sehr viel">Sehr viel</option>
 		</select>
 	</label>
 
 	<br />
 
-	<button class="btn variant-filled-success" type="submit">Jetzt gefüttert</button>
+	<button class="btn variant-filled-success row-span-1" type="submit">Jetzt gefüttert</button>
 </form>
 
 <br />
 
 {#if data.pet.is_inside === true}
-	<h2 class="h3">Ist <b>drinnen!</b></h2>
+	<h2 class="h3">Ist <b>drinnen!</b> Geändert um {getTimeInside()}</h2>
 {:else}
-	<h2 class="h3">Ist <b>draußen!</b></h2>
+	<h2 class="h3">Ist <b>draußen!</b> Geändert um {getTimeInside()}</h2>
 {/if}
 
 <br />
